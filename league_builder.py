f594e3b424fe19b8373d63b00883bb084b89bdff
SaintWacko <saintwacko@gmail.com>
2016-08-20 16:55:03 -0500

    Added debugging

--- league_builder.py
+++ league_builder.py
@@ -3,10 +3,21 @@ import csv
 sharks = []
 dragons = []
 raptors = []
+experienced = []
 
 with open('soccer_players.csv', newline='') as file:
     reader = csv.DictReader(file, delimiter=',')
-    rows = list(reader)
-    for row in rows:
-        print(row)
+    players = list(reader)
+    for player in players:
+        print(player)
+print("\n\n")
 
+for player in players:
+    if player['Soccer Experience'] == 'YES':
+        experienced.append(players.pop(players.index(player)))
+
+for player in experienced:
+    print(player)
+print("\n\n")
+for player in players:
+    print(player)
