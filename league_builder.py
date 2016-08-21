d5b8180b30178d657eb5dc766c71005fe6befcfa
SaintWacko <saintwacko@gmail.com>
2016-08-21 12:21:12 -0500

    Added league building and file writing

--- league_builder.py
+++ league_builder.py
@@ -19,5 +19,34 @@ for player in players:
 for player in experienced:
     print(player)
 print("\n\n")
-for player in players:
-    print(player)
+
+try:
+    while True:
+        sharks.append(experienced.pop())
+        raptors.append(experienced.pop())
+        dragons.append(experienced.pop())
+except(IndexError):
+    pass
+
+try:
+    while True:
+        sharks.append(players.pop())
+        raptors.append(players.pop())
+        dragons.append(players.pop())
+except(IndexError):
+    pass
+
+with open('template', 'r') as file:
+    letter = file.read()
+
+for player in sharks:
+    with open(player['Name'].lower().replace(" ", "_") + ".txt", 'w') as file:
+        file.write(letter.format(**player, league="Sharks", date="March 17 at 3PM"))
+
+for player in dragons:
+    with open(player['Name'].lower().replace(" ", "_") + ".txt", 'w') as file:
+        file.write(letter.format(**player, league="Dragons", date="March 17 at 1PM"))
+
+for player in raptors:
+    with open(player['Name'].lower().replace(" ", "_") + ".txt", 'w') as file:
+        file.write(letter.format(**player, league="Raptors", date="March 18 at 1PM"))
