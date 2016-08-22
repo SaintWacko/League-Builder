f0892d755592077bd3fff0364d169b91f4fa00db
SaintWacko <saintwacko@gmail.com>
2016-08-22 10:21:27 -0500

    Added comments

--- league_builder.py
+++ league_builder.py
@@ -1,3 +1,12 @@
+"""This program is designed to take a csv formated spreadsheet of soccer
+players and distribute them evenly among three teams, ensuring that each
+team has the same number of experienced players.  It then, based on a
+template file, generates form letters to the parents/guardians of each
+player, informing them of the team their child is on and the time of the
+first practice.
+"""
+
+
 import csv
 
 sharks = []
@@ -5,21 +14,17 @@ dragons = []
 raptors = []
 experienced = []
 
+# Read in list of players
 with open('soccer_players.csv', newline='') as file:
     reader = csv.DictReader(file, delimiter=',')
     players = list(reader)
-    for player in players:
-        print(player)
-print("\n\n")
 
-for player in players:
+# Sort experienced players into separate list
+for player in players[:]:
     if player['Soccer Experience'] == 'YES':
         experienced.append(players.pop(players.index(player)))
 
-for player in experienced:
-    print(player)
-print("\n\n")
-
+# spread experienced players evenly between teams
 try:
     while True:
         sharks.append(experienced.pop())
@@ -28,6 +33,7 @@ try:
 except(IndexError):
     pass
 
+# spread inexperienced players evenly between teams
 try:
     while True:
         sharks.append(players.pop())
@@ -36,17 +42,21 @@ try:
 except(IndexError):
     pass
 
+# Read in template from file
 with open('template', 'r') as file:
     letter = file.read()
 
+# Write Sharks letters to files
 for player in sharks:
     with open(player['Name'].lower().replace(" ", "_") + ".txt", 'w') as file:
         file.write(letter.format(**player, league="Sharks", date="March 17 at 3PM"))
 
+# Write Dragons letters to files
 for player in dragons:
     with open(player['Name'].lower().replace(" ", "_") + ".txt", 'w') as file:
         file.write(letter.format(**player, league="Dragons", date="March 17 at 1PM"))
 
+# Write Raptors letters to file
 for player in raptors:
     with open(player['Name'].lower().replace(" ", "_") + ".txt", 'w') as file:
         file.write(letter.format(**player, league="Raptors", date="March 18 at 1PM"))
